e3dc17d6234b1e73e8b6b50dd95bce3a1a3f9a4c
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-21 19:56:13 -0700

    feat: Initial MongoDB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CatsModule } from './cats/cats.module';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     CatsModule,
   ],
   controllers: [AppController],
