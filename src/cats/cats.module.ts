41c37c3f25747d167dfbb40e14bc6c8caa7f54b5
Jerome Taruc <jerometaruc29@gmail.com>
2025-07-21 20:09:42 -0700

    feat: Implement Cat service

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatsResolver } from './cats.resolver';
+import { Cat, CatSchema } from './cats.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+    imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
     providers: [CatsResolver],
 })
 export class CatsModule { }
